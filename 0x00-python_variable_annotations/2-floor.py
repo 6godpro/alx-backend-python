4e78e110d08f34ae0dc6090e5976446758ca2d7b
6godpro <erickson5115@gmail.com>
2023-10-05 21:40:23 +0100

    initial commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A function floor that returns the floor value of the float argument passed.
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor value of a float."""
+    return int(n)
