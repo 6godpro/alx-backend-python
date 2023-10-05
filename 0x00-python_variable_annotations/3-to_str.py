7d19c88ae4ce1fc5148dc01ba125c9f18ecdbcde
6godpro <erickson5115@gmail.com>
2023-10-05 21:44:23 +0100

    initial commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A function to_str that takes a float argument and returns a string
+representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of `n`."""
+    return str(n)
