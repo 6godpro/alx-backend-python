931917231ad8cba3a2d126ef09029770a1d3215f
6godpro <erickson5115@gmail.com>
2023-10-05 23:05:35 +0100

    updated docs and removed NoneType type

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,12 +6,14 @@ def safe_first_element(lst):
     else:
         return None
 """
-from types import NoneType
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
-    """Returns the first element in the iterable"""
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+       Returns the first element inthe
+    iterable or None if lst is not passed.
+    """
     if lst:
         return lst[0]
     else:
