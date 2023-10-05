fb50b511b67dd50116e475dfd93eb93c4ff7f470
6godpro <erickson5115@gmail.com>
2023-10-05 21:30:10 +0100

    initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A function that adds two float values and returns the sum."""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b."""
+    return a + b
