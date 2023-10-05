dc2567615c50acbd6ba91a2e62a66f8271d0de5f
6godpro <erickson5115@gmail.com>
2023-10-05 21:50:16 +0100

    initial commit

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module defines the following variables:
+    a -> int
+    pi -> float
+    i_understand_annotations -> bool
+    school -> string
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
