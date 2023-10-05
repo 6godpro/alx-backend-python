5c6d23b9dbdcc3c5f951466cce5aab3390008189
6godpro <erickson5115@gmail.com>
2023-10-05 21:36:26 +0100

    initial commit

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+A function concat that takes two arguments and returns a concatenated string.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Returns a concatenated string."""
+    return str1 + str2
