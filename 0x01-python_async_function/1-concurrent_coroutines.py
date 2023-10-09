0efaf960cf7a9bb7b9eef6129f87b4a1dfd0bda8
6godpro <erickson5115@gmail.com>
2023-10-09 17:31:59 +0100

    corrected type annotation for return value List[float]

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,12 +5,13 @@ integer arguments. wait_random is spawned n times
 with the specified max_delay.
 """
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a sorted list of all the delays."""
     routines = [wait_random(max_delay) for _ in range(n)]
     return sorted(await asyncio.gather(*routines))
