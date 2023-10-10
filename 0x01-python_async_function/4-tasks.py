b8602b59a0f6f131c597a611f61684ae1453210f
6godpro <erickson5115@gmail.com>
2023-10-10 02:04:23 +0100

    initial commit

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+An  async routine called wait_n that takes in two (2)
+integer arguments. wait_random is spawned n times
+with the specified max_delay.
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a sorted list of all the delays."""
+    routines: List[asyncio.Task] = [task_wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*routines))
