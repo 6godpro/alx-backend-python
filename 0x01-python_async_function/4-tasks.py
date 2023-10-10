762a2dd1482e500f16942dd92c622c78b32e000f
6godpro <erickson5115@gmail.com>
2023-10-10 02:06:20 +0100

    fixed lengthy line

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -13,5 +13,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a sorted list of all the delays."""
-    routines: List[asyncio.Task] = [task_wait_random(max_delay) for _ in range(n)]
+    routines: List[asyncio.Task] = [
+        task_wait_random(max_delay) for _ in range(n)
+    ]
     return sorted(await asyncio.gather(*routines))
