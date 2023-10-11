65ce9213d24a8877af259c93b076c381d47b743e
Eric Eromosele <109639426+6godpro@users.noreply.github.com>
2023-10-11 09:03:02 +0100

    re-fix: added NoneType to return value annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None]:
     """Returns an asynchronous generator."""
     for _ in range(10):
         await asyncio.sleep(1)
