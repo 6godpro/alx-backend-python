a8e1a2b1244c0c379b5018be845532bd44f2fcc7
6godpro <erickson5115@gmail.com>
2023-10-28 09:23:01 +0100

    initial commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Test Suite
+"""
+import json
+from parameterized import parameterized
+from typing import Mapping, Sequence
+
+import unittest
+from unittest.mock import patch, Mock
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test suite for utils.access_nested_map method."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected):
+        """Tests that each parameter set returns the expected output."""
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence):
+        """Tests that each invalid parameter set raises a KeyError."""
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        response = utils.get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, test_payload)
